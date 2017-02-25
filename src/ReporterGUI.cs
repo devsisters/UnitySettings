00fe1c0c9458076d6c1b3a756ac5e47dbbb65bd9
김대원 <eodnjs2998@gmail.com>
2017-02-26 00:09:56 +0900

    csproj, src 추가.

---
+++ src/ReporterGUI.cs
@@ -0,0 +1,54 @@
+﻿#if DEBUG
+using System;
+using UnityEngine;
+
+public class ReporterGUI : MonoBehaviour {
+	public static class Events {
+		public static Func<string> getBuildInfo;
+		public static Action<ReporterGUI> onStart;
+		public static Action<ReporterGUI> onDestroy;
+		public static Action<ReporterGUI> onEnable;
+		public static Action<ReporterGUI> onDisable;
+	}
+
+	public static ReporterGUI inst;
+
+	Reporter reporter ;
+
+	void Awake()
+	{
+		if (inst == null) inst = this;
+		reporter = gameObject.GetComponent<Reporter>();
+	}
+
+	void Start()
+	{
+		if (Events.onStart != null)
+			Events.onStart(this);
+	}
+
+	void OnDestroy()
+	{
+		if (inst == this) inst = null;
+		if (Events.onDestroy != null)
+			Events.onDestroy(this);
+	}
+
+	void OnEnable()
+	{
+		if (Events.onEnable != null)
+			Events.onEnable(this);
+	}
+
+	void OnDisable()
+	{
+		if (Events.onDisable != null)
+			Events.onDisable(this);
+	}
+
+	void OnGUI()
+	{
+		reporter.OnGUIDraw();
+	}
+}
+#endif
---
+++ src/ReporterMessageReceiver.cs
@@ -0,0 +1,45 @@
+﻿#if DEBUG
+using UnityEngine;
+
+public class ReporterMessageReceiver : MonoBehaviour 
+{
+	Reporter reporter ;
+	void Start()
+	{
+		reporter = gameObject.GetComponent<Reporter>();
+	}
+
+	void OnPreStart(){
+		//To Do : this method is called before initializing reporter, 
+		//we can for example check the resultion of our device ,then change the size of reporter
+		if( reporter == null )
+			reporter = gameObject.GetComponent<Reporter>();
+
+		if( Screen.width < 1000 )
+			reporter.size = new Vector2( 32 , 32 );
+		else 
+			reporter.size = new Vector2( 48 , 48);
+
+		reporter.GetUserData = GetUserData;
+	}
+	void OnHideReporter()
+	{
+		//TO DO : resume your game
+	}
+
+	void OnShowReporter()
+	{
+		//TO DO : pause your game and disable its GUI
+	}
+
+	void OnLog( Reporter.Log log )
+	{
+		//TO DO : put you custom code 
+	}
+	
+	private static string GetUserData()
+	{
+		return "IpAddress: " + Network.player.ipAddress;
+	}
+}
+#endif
