43e75eddce208f44e16a9050fd2d92d39df2c71e
김대원 <james.kim@devsisters.com>
2017-03-09 23:45:54 +0900

    implement keyboard binding.

--- src/Log/LogGUIView.Keyboard.cs
+++ src/Log/LogGUIView.Keyboard.cs
@@ -1,49 +1,73 @@
 using UnityEngine;
+using K = UnityEngine.KeyCode;
 
 namespace Settings.Log
 {
     internal partial class GUIView : GUI.IView
     {
-        private const float _upDownCoolTimeFirst = 0.3f;
-        private const float _upDownCoolTimeFast = 0.02f;
         private float _upDownTimeLeft;
+        private K _curUpDownKey;
+        private readonly K[] _upDownKeys = new[] {
+            K.UpArrow, K.DownArrow,
+            K.PageUp, K.PageDown,
+            K.Home, K.End,
+        };
 
-        private void UpSelectedLog(float cooltime)
+        private static int UpDownAmountForKey(K key)
         {
-            _keepInSelectedLog = true;
-            _upDownTimeLeft = cooltime;
-            --_selectedLog;
+            switch (key)
+            {
+                case K.UpArrow: return -1;
+                case K.DownArrow: return 1;
+                case K.PageUp: return -5;
+                case K.PageDown: return 5;
+                case K.Home: return -10000;
+                case K.End: return 10000;
+                default: return 0;
+            }
         }
 
-        private void DownSelectedLog(float cooltime)
+        private void UpDownSelectedLogWithCurKey(float cooltime)
         {
             _keepInSelectedLog = true;
+            _selectedLog += UpDownAmountForKey(_curUpDownKey);
             _upDownTimeLeft = cooltime;
-            ++_selectedLog;
+        }
+
+        private void CheckAndUpdateSelectedLog(K key)
+        {
+            const float coolTimeFirst = 0.3f;
+            if (_curUpDownKey != key && (Input.GetKeyDown(key) || Input.GetKey(key)))
+            {
+                _curUpDownKey = key;
+                UpDownSelectedLogWithCurKey(coolTimeFirst);
+            }
+            else if (_curUpDownKey == key && (Input.GetKeyUp(key) || !Input.GetKey(key)))
+            {
+                _curUpDownKey = K.None;
+                _upDownTimeLeft = coolTimeFirst;
+            }
         }
 
         private void UpdateKeyboardAction()
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-                UpSelectedLog(_upDownCoolTimeFirst);
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
-                DownSelectedLog(_upDownCoolTimeFirst);
-            else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
-                _upDownTimeLeft = 0;
+            foreach (var key in _upDownKeys)
+                CheckAndUpdateSelectedLog(key);
         }
 
         private void UpdateKeyboardStay()
         {
+            if (_curUpDownKey == K.None)
+                return;
+
             if (_upDownTimeLeft > 0)
             {
                 _upDownTimeLeft -= Time.unscaledDeltaTime;
                 return;
             }
 
-            else if (Input.GetKey(KeyCode.UpArrow))
-                UpSelectedLog(_upDownCoolTimeFast);
-            else if (Input.GetKey(KeyCode.DownArrow))
-                DownSelectedLog(_upDownCoolTimeFast);
+            const float coolTimeFast = 0.02f;
+            UpDownSelectedLogWithCurKey(coolTimeFast);
         }
 
         private void UpdateKeyboard()
