4690c8593267ddb4514c9e6d3e92880adea3166e
김대원 <james.kim@devsisters.com>
2017-03-18 16:18:44 +0900

    add example

---
+++ example/Assets/Main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        Settings.Installer.Install();
+        // Invoke("LogSame", 0.1f);
+        // Invoke("Log", 0.1f);
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(CoroutineLog());
+    }
+
+    void LogSame()
+    {
+        for (var i = 0; i != 40; ++i)
+            Debug.Log("some message some message some message");
+    }
+
+    void Log()
+    {
+        for (var i = 0; i != 40; ++i)
+            Debug.Log(i + "some message some message some message");
+    }
+
+    private System.Collections.IEnumerator CoroutineLog()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            Debug.Log(Time.realtimeSinceStartup.ToString("0.000") + " coroutine log");
+        }
+    }
+}
